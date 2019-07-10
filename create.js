030bbf4bc5cc9323e0a0f05a7f55fe32776154b6
chriscauley <chris@lablackey.com>
2019-07-10 02:37:42 +0000

    f1rst

---
+++ create.js
@@ -0,0 +1,35 @@
+// Usage uR.element.create("my-tag",attrs,options)
+// element = <attrs.parent><my-tag {...attrs}></attrs.parent>
+// options && riot.mount(element,options)
+
+import riot from 'riot'
+import emptyElement from './emptyElement'
+
+export default (tagName, attrs, riot_opts) => {
+  const element = document.createElement(tagName)
+  if (attrs.parent) {
+    if (typeof attrs.parent === 'string') {
+      attrs.parent = document.querySelector(attrs.parent)
+    }
+    if (attrs.clear) {
+      // #! TODO there's probably a better way to handle this
+      // technically clear isn't an attribute, but we need a way to empty the parent
+      emptyElement(attrs.parent)
+      delete attrs.clear
+    }
+    attrs.parent.appendChild(element)
+    delete attrs.parent
+  }
+
+  Object.assign(element, attrs)
+
+  if (riot_opts) {
+    try {
+      riot.mount(element, riot_opts)
+    } catch (e) {
+      console.error("unable to mount",element,riot_opts)
+      throw e
+    }
+  }
+  return element
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+import create from './create'
+import emptyElement from './emptyElement'
+import alert from './alert'
+import config from './config'
+import text2obj from './text2obj'
+
+export default {
+  alert,
+  config,
+  create,
+  emptyElement,
+  text2obj,
+}
